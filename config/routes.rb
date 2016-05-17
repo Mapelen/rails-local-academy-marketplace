9fcc0762de4d335c2d748fe643f2596fd0d98616
Romain <romain.cky@gmail.com>
2016-05-17 12:32:39 +0200

    added routes for expeditions & destinations in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
- get 'home' => 'pages#home'
+  get 'home' => 'pages#home'
+  resources :expeditions
+  resources :destinations
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

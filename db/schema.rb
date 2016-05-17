cb6c2adc43fdfd35f9b15f2cf2ab0091471fea64
Romain <romain.cky@gmail.com>
2016-05-17 11:43:32 +0200

    retrieved user and added reference in expeditions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517093450) do
+ActiveRecord::Schema.define(version: 20160517094251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +36,11 @@ ActiveRecord::Schema.define(version: 20160517093450) do
     t.integer  "destination_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
 
   add_index "expeditions", ["destination_id"], name: "index_expeditions_on_destination_id", using: :btree
+  add_index "expeditions", ["user_id"], name: "index_expeditions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -59,4 +61,5 @@ ActiveRecord::Schema.define(version: 20160517093450) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "expeditions", "destinations"
+  add_foreign_key "expeditions", "users"
 end

e2d66f4423d651a08a3cd87a85eeededdc0d4d5d
Romain <romain.cky@gmail.com>
2016-05-17 11:56:19 +0200

    Added validations for Expedition & Destination models

--- app/models/expedition.rb
+++ app/models/expedition.rb
@@ -1,3 +1,11 @@
 class Expedition < ActiveRecord::Base
   belongs_to :destination
+
+  validates :title, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :start_on, presence: true
+  validates :end_on, presence: true
+  validates :capacity, presence: true
+  validates :theme, presence: true
+  validates :destination, presence: true
 end

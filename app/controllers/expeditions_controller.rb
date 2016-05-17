2da7f7e5eb0a59a115f4690ee012832dcd7a9474
Romain <romain.cky@gmail.com>
2016-05-17 12:29:50 +0200

    made methods in Expeditions Controller

--- app/controllers/expeditions_controller.rb
+++ app/controllers/expeditions_controller.rb
@@ -1,2 +1,48 @@
 class ExpeditionsController < ApplicationController
+  before_action :set_params, only: [:create, :edit, :update]
+
+  def index
+    @expeditions = Expedition.all
+  end
+
+  def show
+    @expedition = find_expedition
+  end
+
+  def new
+    @expedition = Expedition.new
+  end
+
+  def create
+    @expedition = Expedition.new(set_expedition)
+    if @expedition.save
+      redirect_to expedition_path(@expedition)
+    else
+      render "expeditions/new"
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @expedition = find_expedition
+    @expedition.destroy
+    redirect_to expeditions_path
+  end
+
+  private
+
+  def set_params
+    params.require(:expedition).permit(:title, :photo, :photo_cache)
+  end
+
+  def find_expedition
+    @expedition = Expedition.find(params[:id])
+  end
 end

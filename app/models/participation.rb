2a92a636196115712cf134b2dbdad12070965dc5
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-18 15:00:36 +0200

    Fin du form d'inscription pour une lex

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,5 +1,6 @@
 class Participation < ActiveRecord::Base
   belongs_to :user
   belongs_to :expedition
+  validates :user_id, :uniqueness => { :scope => :expedition_id }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :participations
 end

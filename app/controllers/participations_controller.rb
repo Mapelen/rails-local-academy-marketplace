a7bbabd5358b8f246e1638ad6ccf3c54df8a7d08
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-19 16:33:26 +0200

    add the opt out

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,18 +1,24 @@
 class ParticipationsController < ApplicationController
+  before_action :find_expedition, only: [:new, :create]
 
   def new
     @participation = Participation.new
-
   end
-
 
   def create
     @participation = Participation.new(participation_params)
     if @participation.save
-      redirect_to expedition_path(find_expedition)
+      redirect_to expedition_path(@expedition)
     else
       render "expeditions/new"
     end
+  end
+
+  def destroy
+    @participation = Participation.find(params[:id])
+    @expedition = @participation.expedition
+    @participation.destroy
+    redirect_to expedition_path(@expedition)
   end
 
   private
@@ -22,7 +28,6 @@ class ParticipationsController < ApplicationController
   end
 
   def find_expedition
-  @expedition = Expedition.find(@participation.expedition_id)
+    @expedition = Expedition.find(params[:expedition_id])
   end
-
 end

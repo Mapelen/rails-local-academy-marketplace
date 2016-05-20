1cc9157d19fab17ceb92ba62d50610c493031a30
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-20 12:15:59 +0200

    beginning of booking-management

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519145236) do
+ActiveRecord::Schema.define(version: 20160520094816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160519145236) do
     t.boolean  "is_company"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.boolean  "accepted"
   end
 
   add_index "participations", ["expedition_id"], name: "index_participations_on_expedition_id", using: :btree
